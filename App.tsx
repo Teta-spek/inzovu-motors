1d4fc7afb002a8de4fa5da246671625eb11ad7b6
Teta spek <labs@spek.world>
2026-05-23 22:51:37 -0400

    Update App.tsx via Afrodesign

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import Hero from './components/Hero';
+import WhySection from './components/WhySection';
+
+export default function App() {
+  return (
+    <div>
+      <Hero />
+      <WhySection />
+    </div>
+  );
+}
